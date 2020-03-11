6d0c258cfae8cb41e153016340a0b14e266bab00
syxxcalypso <jen.sanchez1404@gmail.com>
2020-03-10 21:19:45 -0600

    .

--- src/matrix.py
+++ src/matrix.py
@@ -1,8 +1,4 @@
-try:
-    import pymp
-except ImportError:
-    print("PyMP Failed to import, is it install?")
-    sys.exit(1)
+import numpy # Required by mpi4py so cannot reasonably be considered an additional library!
 
 class MatrixIncompatibleError(Exception):
     def __init__(self, message):
@@ -10,43 +6,30 @@ class MatrixIncompatibleError(Exception):
 
 class ParallelMatrix(object):
     __doc__ = ''''''
-    def __init__(self, rows, cols, pthreads=1, fill_random=False):
+    def __init__(self, rows, cols, fill_random=False):
         self.rows = rows
         self.cols = cols
-        self.table = pymp.shared.array((rows,cols), dtype='int')
+        self.table = numpy.array((rows,cols), dtype=numpy.short)
         if fill_random:
             for i in range(self.rows):
                 for j in range(self.cols):
                     self.table[i][j] = random.randint(1,6)
 
     @classmethod
-    def from_list(cls, multilist, pthreads=1):
+    def from_list(cls, multilist):
         cols = len(multilist[0])
         for row in multilist:
             if len(row) != cols:
                 raise TypeError("Multidimensional list must be rectangular!")
-        m = cls(len(multilist), cols, pthreads)
-        for i in range(m.rows):
-            for j in range(m.cols):
-                m.table[i][j] = multilist[i][j]
+        m = cls(len(multilist), cols)
+        m.table = numpy.array(multilist, dtype=numpy.short)
         return m
-
-    def __matmul__(self, obj):
-        if not self.rows == obj.cols:
-            raise MatrixIncompatibleError("Matrix row count does not match target Matrix column count!\n")
-        result_matrix = Matrix(self.rows, obj.cols)
-        with pymp.Parallel(pthreads) as p:
-            for _row in p.range(self.rows):
-                for col in range(obj.cols):
-                    for row in range(obj.rows):
-                        result_matrix.table[_row][col] += self.table[_row][row] * obj.table[row][col]
-        return result_matrix
 
     def __str__(self):
         result_matrix_s = ''
         for row in self.table:
             for i in row:
-                if i >= 2**16:
+                if i >= 2**10:
                     result_matrix_s += ' . '
                 else:
                     result_matrix_s += '{:2d} '.format(i)
--- src/path_reduction.py
+++ src/path_reduction.py
@@ -8,16 +8,17 @@
 ####################################
 import os, sys, time, matrix
 
-def path_reduce(w, d):
-    for k in range(w.rows):
-        for i in range(w.cols):
-            for j in range(w.rows):
-                d.table[i][j] = min(d.table[i][j], d.table[i][k] + d.table[k][j])
+try:
+    from mpi4py import MPI
+except ImportError:
+    print("OpenMPI Failed to import, is it installed?")
+    sys.exit(1)
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
 
-#--- Some arbitrary and very large integer
-z = 2**16
-
+#--- Some arbitrary and very large integer weight
+z = 2**10
 #--- Weighted symmetrical graph
 #         0   1   2   3   4   5   6   7
 #         A   B   C   D   E   F   G   H
@@ -29,20 +30,35 @@ graph = [[0,  4,  z,  z,  z,  z,  7,  4],  # A  0
          [z,  z,  z,  z,  6,  0,  z,  z],  # F  5
          [z,  4,  z,  z,  z,  7,  0,  z],  # G  6
          [z,  z,  3,  z,  z,  z,  z,  0]]  # H  5
+W = matrix.ParallelMatrix.from_list(graph)
+D = matrix.ParallelMatrix.from_list(graph)
 
+def path_reduce(d):
+    for k in range(d.rows):
+        for i in range(d.cols):
+            for j in range(d.rows):
+                d.table[i][j] = min(d.table[i][j], d.table[i][k] + d.table[k][j])
 
-
-#--- Create weight and solution matrices from graph
-W = matrix.ParallelMatrix.from_list(graph, 2)
-D = matrix.ParallelMatrix.from_list(graph, 2)
+def path_reduce_multi(d):
+    for k in range(d.rows):
+        d[k] = comm.bcast(d[k], root=0)
+        for i in range(d.cols):
+            for j in range(d.rows):
+                d.table[i][j] = min(d.table[i][j], d.table[i][k] + d.table[k][j])
 
 if __name__ == '__main__':
+    '''
+    if rank == 0:
+        for k in range(D.rows):
+            D[k] = comm.recv(source=0, tag=k)
+    else:
+        for k in range(D.rows):
+            comm.Send(D[k], dest=0, tag=k)
+    '''
     start = time.time()
-    path_reduce(W, D)
+    path_reduce(D)
     end = time.time()
     print(W)
     print('   Example Transform\n          |\n          v\n\n')
     print(D)
     print('Time Taken: %.20fs' % (end-start))
-
-# At this commit stage, the program is still using pymp, please look to further commits for use of mpi
